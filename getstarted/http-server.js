158ba8dbeaebb5ddd995d8a48892098f5c29d8ae
cfsghost <cfsghost@gmail.com>
2016-10-17 23:48:33 -0400

    added http server example

---
+++ getstarted/http-server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+// Create a HTTP server
+var server = http.createServer(function (req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hello World\n');
+});
+
+// Listen on 3000 port
+server.listen(3000, function() {
+	console.log('Server running at http://localhost/');
+});
